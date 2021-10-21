c3f978a0fbf74b60c4ac287b168d5268ae1862ee
Cesar Tavares <63663846+CesarTavares@users.noreply.github.com>
2021-10-21 10:27:41 -0300

    criação da aula JavaScript

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+//lista de dicionarios
+var frutas = [{nome:"maça", cor: "vermelha"},{nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+/*//dicionario
+var fruta = {nome:"maça", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+/*var nome = "Cesar Ricardo Tavares";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo!!";
+//alert("Bem Vindo! " + nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log((n1 * n2)/3);
+console.log(frase.toLocaleLowerCase());
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));*/
+//var lista = ["Maça", "Pêra", "Laranja"];
+//lista.push("Uva"); // push para adicionar elementos a minha lista
+//lista.pop();// pop para retirar o elemento da lista
+//console.log(lista);
+//console.log(lista.length);//quantidade de elementos que tenho na minha lista
+//console.log(lista.reverse());//reverse para inverter a posição na lista
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
